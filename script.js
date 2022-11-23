858b6011ba4f48d1a457fcfd802ed5d032b98b7b
Jesse Kucharek <111667621+jkucharek@users.noreply.github.com>
2022-11-23 11:23:28 -0800

    Added code for triangles and character swapping

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+	//establish whether device viewing page is mobile or desktop
 	if(window.innerWidth >= 1024) {
 		document.getElementById("desktopMain").hidden = false;
 		document.getElementById("mobileMain").hidden = true;
@@ -9,6 +10,8 @@ window.onload = function() {
 
 	
 	
+
+	//Code for navigation accordion on mobile version
 	document.getElementsByTagName('nav')[0].style.maxHeight = "0px";
 
 	document.getElementById('hamburger').addEventListener("click", function() {
@@ -23,28 +26,67 @@ window.onload = function() {
 
 
 
-	//set height of all panel elements to 0
+	//Code for the accordion on FAQ page
 	for (let i = 0; i < 4; i++) {
+		//set height of all accordion panels to 0
 		document.getElementsByClassName('panel')[i].style.maxHeight = "0px";
 
+
+		//establish event listener for click
 		document.getElementsByClassName('accordion')[i].addEventListener("click", function() {
+			
+			//expand panel if collapsed
 			if(document.getElementsByClassName('panel')[i].style.maxHeight == "0px") {
 				document.getElementsByClassName('panel')[i].style.maxHeight = document.getElementsByClassName('panel')[i].scrollHeight + "px";
+				document.getElementsByClassName('accordion')[i].classList.add("active");
+
+				//and collapse all other panels
 				for (let j = 0; j < 4; j++) {
-					if (j != i) 
+					if (j != i) { 
 						document.getElementsByClassName('panel')[j].style.maxHeight = "0px"
+						document.getElementsByClassName('accordion')[j].classList.remove("active");
+					}
 				}
 			}
-			else
+			//collapse panel if expanded
+			else {
 				document.getElementsByClassName('panel')[i].style.maxHeight = "0px";
+				document.getElementsByClassName('accordion')[i].classList.remove("active");
+			}
 		});
 	}
 };
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//code for the cart
 function loadCart() {
 	window.location = "cart.html";
 }
 
+
+
+
+
+
+
+
+
+
+
+//Make the search box actually query Google
 function mobileQuery() {
 	window.location = "https://www.google.com/search?q=" + document.getElementById("searchField").value;
 }
@@ -53,6 +95,14 @@ function desktopQuery() {
 	window.location = "https://www.google.com/search?q=" + document.getElementById("desktopSearchField").value;
 }
 
+
+
+
+
+
+
+
+//switch between mobile or desktop main sections on resize
 window.onresize = function() {
 	if(window.innerWidth >= 1024) {
 		document.getElementById("desktopMain").hidden = false;
